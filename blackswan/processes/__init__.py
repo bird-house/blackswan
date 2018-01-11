00f0d33f98facbb57e9e8bf05a2d8aafffc7212d
Nikolay Kadygrov <nkadyg@lsce3240.lsce.ipsl.fr>
2018-01-11 18:08:40 +0100

    fix env madness

--- blackswan/processes/__init__.py
+++ blackswan/processes/__init__.py
@@ -12,13 +12,13 @@
 #from .wps_sdm_csv import SDMcsvProcess
 #from .wps_sdm_csvindices import SDMcsvindicesProcess
 #from .wps_sdm_allinone import SDMallinoneProcess
-from .wps_weatherregimes_reanalyse import WeatherregimesreanalyseProcess
-from .wps_weatherregimes_projection import WeatherregimesprojectionProcess
-from .wps_weatherregimes_model import WeatherregimesmodelProcess
-from .wps_analogs_reanalyse import AnalogsreanalyseProcess
-from .wps_analogs_model import AnalogsmodelProcess
-from .wps_analogs_compare import AnalogscompareProcess
-from .wps_analogs_viewer import AnalogsviewerProcess
+#from .wps_weatherregimes_reanalyse import WeatherregimesreanalyseProcess
+#from .wps_weatherregimes_projection import WeatherregimesprojectionProcess
+#from .wps_weatherregimes_model import WeatherregimesmodelProcess
+#from .wps_analogs_reanalyse import AnalogsreanalyseProcess
+#from .wps_analogs_model import AnalogsmodelProcess
+#from .wps_analogs_compare import AnalogscompareProcess
+#from .wps_analogs_viewer import AnalogsviewerProcess
 # from .wps_robustness import RobustnessProcess
 #from .wps_plot_timeseries import PlottimeseriesProcess
 #from .wps_segetalflora import SegetalfloraProcess
@@ -46,13 +46,13 @@ processes = [
     # SDMcsvindicesProcess(),
     # SDMcsvProcess(),
     # SDMallinoneProcess(),
-    WeatherregimesreanalyseProcess(),
-    WeatherregimesprojectionProcess(),
-    WeatherregimesmodelProcess(),
-    AnalogsreanalyseProcess(),
-    AnalogsmodelProcess(),
-    AnalogscompareProcess(),
-    AnalogsviewerProcess(),
+#    WeatherregimesreanalyseProcess(),
+#    WeatherregimesprojectionProcess(),
+#    WeatherregimesmodelProcess(),
+#    AnalogsreanalyseProcess(),
+#    AnalogsmodelProcess(),
+#    AnalogscompareProcess(),
+#    AnalogsviewerProcess(),
     # RobustnessProcess(),
     # PlottimeseriesProcess(),
     # SegetalfloraProcess(),
