88869298cb7a77d650749fecb38e1bb99d5d9ef0
nkadygrov <kadygrov@gmail.com>
2018-07-12 13:23:15 +0200

    add Rconfig.R

--- Rconfig.R
+++ Rconfig.R
@@ -8,6 +8,6 @@ repos = "https://cloud.r-project.org"
 
 install.packages("evmix",  dependencies = TRUE, repos = repos)
 install.packages("alabama",  dependencies = TRUE, repos = repos)
-install.packages("blackswan/Rsrc/FARg_0.1.tar.gz", lib = lib)
+install.packages("blackswan/Rsrc/FARg_0.1.tar.gz")
 install.packages("fastmatch",  dependencies = TRUE, repos = repos)
-install.packages("blackswan/Rsrc/FARallnat_0.1.tar.gz", lib = lib)
+install.packages("blackswan/Rsrc/FARallnat_0.1.tar.gz")
