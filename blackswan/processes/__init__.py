2d19b79db0797bcbadf8b1aa0e7b58c1cb6a18a9
Nikolay Kadygrov <nkadyg@lsce3240.lsce.ipsl.fr>
2019-01-23 14:54:51 +0100

    add analogs for models

--- blackswan/processes/__init__.py
+++ blackswan/processes/__init__.py
@@ -1,6 +1,8 @@
 from .wps_say_hello import SayHello
 from .wps_analogs_reanalyse import AnalogsreanalyseProcess
+from .wps_analogs_model import AnalogsmodelProcess
 processes = [
     SayHello(),
     AnalogsreanalyseProcess(),
+    AnalogsmodelProcess(),
 ]
