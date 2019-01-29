015b0d7321a9e69e20b4c8566bb20d00382f6f9e
Nikolay Kadygrov <nkadyg@lsce3240.lsce.ipsl.fr>
2019-01-29 16:03:40 +0100

    add attribution processs

--- blackswan/processes/__init__.py
+++ blackswan/processes/__init__.py
@@ -1,8 +1,10 @@
 from .wps_say_hello import SayHello
 from .wps_analogs_reanalyse import AnalogsreanalyseProcess
 from .wps_analogs_model import AnalogsmodelProcess
+from .wps_pythonanattribution import PythonanattributionProcess
 processes = [
     SayHello(),
     AnalogsreanalyseProcess(),
     AnalogsmodelProcess(),
+    PythonanattributionProcess(),
 ]
